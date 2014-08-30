3a43dcb1077a11a345f746ba4f65642ba44d2b1f
Bob Lail <bob.lail@cph.org>
2014-08-30 15:27:04 -0500

    [fix] Removed illegal return from navigation renderer (1m)

--- config/initializers/add_navigation_renderer.rb
+++ config/initializers/add_navigation_renderer.rb
@@ -1,12 +1,12 @@
 Houston.config.add_navigation_renderer :roadmap do
   projects = followed_projects.select { |project| can?(:read, project.milestones.build) }
-  return nil if projects.empty?
-  
-  menu_items = []
-  menu_items << MenuItem.new("All", Houston::Roadmap::Engine.routes.url_helpers.project_roadmaps_path)
-  menu_items << MenuItemDivider.new
-  menu_items.concat projects.map { |project| ProjectMenuItem.new(project, Houston::Roadmap::Engine.routes.url_helpers.project_roadmap_path(project)) }
-  menu_items
-  
-  render_nav_menu "Roadmap", icon: "fa-road", items: menu_items
+  unless projects.empty?
+    menu_items = []
+    menu_items << MenuItem.new("All", Houston::Roadmap::Engine.routes.url_helpers.project_roadmaps_path)
+    menu_items << MenuItemDivider.new
+    menu_items.concat projects.map { |project| ProjectMenuItem.new(project, Houston::Roadmap::Engine.routes.url_helpers.project_roadmap_path(project)) }
+    menu_items
+    
+    render_nav_menu "Roadmap", icon: "fa-road", items: menu_items
+  end
 end
