2076e11cebf4b84c5a918a156c3a03daa716098a
Bob Lail <bob.lail@cph.org>
2014-08-30 11:53:54 -0500

    [refactor] Changed how Houston specifies navigation (5m)

--- lib/houston/roadmap.rb
+++ lib/houston/roadmap.rb
@@ -4,22 +4,5 @@ module Houston
   module Roadmap
     extend self
     
-    
-    def menu_items_for(context={})
-      projects = context[:projects]
-      ability = context[:ability]
-      user = context[:user]
-      
-      projects = projects.select { |project| ability.can?(:read, project.milestones.build) }
-      return [] if projects.empty?
-      
-      menu_items = []
-      menu_items << MenuItem.new("All", Engine.routes.url_helpers.project_roadmaps_path)
-      menu_items << MenuItemDivider.new
-      menu_items.concat projects.map { |project| ProjectMenuItem.new(project, Engine.routes.url_helpers.project_roadmap_path(project)) }
-      menu_items
-    end
-    
-    
   end
 end
