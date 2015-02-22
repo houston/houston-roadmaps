98ad8d48123ff28f20134ae45b2bafe8f3837b7d
Bob Lail <bob.lail@cph.org>
2015-02-22 16:59:39 -0600

    [refactor] Refactored Navigation Renderer (1m)

--- config/initializers/add_navigation_renderer.rb
+++ config/initializers/add_navigation_renderer.rb
@@ -1,12 +1,13 @@
 Houston.config.add_navigation_renderer :roadmap do
-  projects = followed_projects.select { |project| can?(:read, project.milestones.build) }
-  unless projects.empty?
-    menu_items = []
-    menu_items << MenuItem.new("All", Houston::Roadmap::Engine.routes.url_helpers.project_roadmaps_path)
-    menu_items << MenuItemDivider.new
-    menu_items.concat projects.map { |project| ProjectMenuItem.new(project, Houston::Roadmap::Engine.routes.url_helpers.project_roadmap_path(project)) }
-    menu_items
-    
-    render_nav_menu "Roadmap", icon: "fa-road", items: menu_items
+  if can?(:read, Milestone)
+    render_nav_link "Roadmap", Houston::Roadmap::Engine.routes.url_helpers.project_roadmaps_path, icon: "fa-road"
   end
+end
+
+
+Houston.config.add_project_feature :roadmap do
+  name "Roadmap"
+  icon "fa-road"
+  path { |project| Houston::Roadmap::Engine.routes.url_helpers.project_roadmap_path(project) }
+  ability { |ability, project| ability.can?(:read, project.milestones.build) }
 end
