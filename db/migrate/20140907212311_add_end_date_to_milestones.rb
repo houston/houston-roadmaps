20e3f2d7325d1bf841b1ce09157dd16b885362d6
Bob Lail <bob.lail@cph.org>
2015-10-25 20:26:45 -0500

    [skip] Skipped an old data migration if there are no milestones (1m)

--- db/migrate/20140907212311_add_end_date_to_milestones.rb
+++ db/migrate/20140907212311_add_end_date_to_milestones.rb
@@ -1,13 +1,16 @@
 class AddEndDateToMilestones < ActiveRecord::Migration
   def up
     add_column :milestones, :end_date, :date
-    execute <<-SQL
-      UPDATE milestones
-        SET end_date=start_date + (size || ' ' || units)::interval - '2 days'::interval
-      WHERE start_date IS NOT NULL
-        AND size IS NOT NULL
-        AND units IS NOT NULL
-    SQL
+
+    if Milestone.count > 0
+      execute <<-SQL
+        UPDATE milestones
+          SET end_date=start_date + (size || ' ' || units)::interval - '2 days'::interval
+        WHERE start_date IS NOT NULL
+          AND size IS NOT NULL
+          AND units IS NOT NULL
+      SQL
+    end
   end
 
   def down
