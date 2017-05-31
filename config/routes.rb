86c536e6502886a2b524f45a331bd15b0b4bd9ed
Bob Lail <bob.lailfamily@gmail.com>
2017-05-31 08:44:38 -0500

    [skip] Used Ruby 1.9-style hashes (1m)

--- config/routes.rb
+++ config/routes.rb
@@ -19,21 +19,21 @@ Houston::Roadmaps::Engine.routes.draw do
   end
 
   scope "roadmap" do
-    post "milestones", :to => "milestones#create"
-    get "milestones/:id", :to => "milestones#show", :as => :milestone
-    put "milestones/:id", :to => "milestones#update"
-    put "milestones/:id/ticket_order", :to => "milestones#update_order"
+    post "milestones", to: "milestones#create"
+    get "milestones/:id", to: "milestones#show", as: :milestone
+    put "milestones/:id", to: "milestones#update"
+    put "milestones/:id/ticket_order", to: "milestones#update_order"
 
-    post "milestones/:id/tickets", :to => "milestones#create_ticket", constraints: {id: /\d+/}
-    post "milestones/:id/tickets/:ticket_id", :to => "milestones#add_ticket", constraints: {id: /\d+/, ticket_id: /\d+/}
-    delete "milestones/:id/tickets/:ticket_id", :to => "milestones#remove_ticket", constraints: {id: /\d+/, ticket_id: /\d+/}
+    post "milestones/:id/tickets", to: "milestones#create_ticket", constraints: {id: /\d+/}
+    post "milestones/:id/tickets/:ticket_id", to: "milestones#add_ticket", constraints: {id: /\d+/, ticket_id: /\d+/}
+    delete "milestones/:id/tickets/:ticket_id", to: "milestones#remove_ticket", constraints: {id: /\d+/, ticket_id: /\d+/}
 
-    post "milestones/:id/upgrade", :to => "milestones#upgrade", constraints: {id: /\d+/}
+    post "milestones/:id/upgrade", to: "milestones#upgrade", constraints: {id: /\d+/}
 
 
-    post "goals", :to => "goals#create"
-    get "goals/:id", :to => "goals#show", :as => :goal
-    put "goals/:id", :to => "goals#update"
+    post "goals", to: "goals#create"
+    get "goals/:id", to: "goals#show", as: :goal
+    put "goals/:id", to: "goals#update"
 
     put "goals/:goal_id/todolists/:id", to: "goal_todo_lists#add"
     delete "goals/:goal_id/todolists/:id", to: "goal_todo_lists#remove"
